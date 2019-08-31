1ec3acb53ff592d9dc8bfa7202c90c249470ff4d
devs4j <53630823+devs4j@users.noreply.github.com>
2019-08-31 00:08:38 -0500

    Update HelloWorldApplication.java

--- src/main/java/mx/com/devs4j/microservices/HelloWorldApplication.java
+++ src/main/java/mx/com/devs4j/microservices/HelloWorldApplication.java
@@ -12,7 +12,7 @@ public class HelloWorldApplication {
 	
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello World";
+		return "Hello World Vato";
 	}
 
 	public static void main(String[] args) {
